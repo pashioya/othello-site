02415a1dc661e5b6da2ffc6a39b7e7480acf6d04
Margarita <margarita.stoyanova@student.kdg.be>
2022-05-25 19:43:42 +0200

    Log in added

---
+++ JavaScript/users.js
@@ -0,0 +1,22 @@
+export const usersJson = [
+  {
+    "username": "1",
+    "password": "1",
+    "role": "user"
+  },
+  {
+    "username": "Endellos",
+    "password": "sollednE",
+    "role": "admin"
+  },
+  {
+    "username": "Magi",
+    "password": "igaM",
+    "role": "user"
+  },
+  {
+    "username": "A",
+    "password": "A",
+    "role": "user"
+  }
+]
